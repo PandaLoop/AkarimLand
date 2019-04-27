2ae7ef29fc22d1fefa877185c6383ee1dbfc8d8b
Panda <45695790+PandaLoop@users.noreply.github.com>
2019-04-27 18:41:27 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+﻿<html>
+<head>
+
+<title> Las tierras de Arkim </title>
+<script language="javascript">
+alert("Bienvenido a las de Akarim joven explorador de la web, aquí encontrarás una increible aventura. Aunque se desea mejorar la experiencia del usuario aquí no se guardan cookis por lo tanto no es necesario aceptar nada para echar un vistazo");
+
+</script>
+</head>
+<body>
+<img src="small3.jpeg">
+
+</body>
+</html>
